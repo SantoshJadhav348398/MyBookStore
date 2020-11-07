467c891d0ce6be2acc2a6765a8dc6a40ae8d1948
Reena <94reena.singh@gmail.com>
2020-11-08 00:08:16 +0530

    making css changes

--- MyBookStoreApp/src/app/sign-up-login/sign-up-login.module.ts
+++ MyBookStoreApp/src/app/sign-up-login/sign-up-login.module.ts
@@ -4,15 +4,22 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { RouterModule } from '@angular/router';
 import { SignuploginRoutingModule } from './signuplogin-routing.module';
-
-
-
+import { MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [LoginComponent, SignUpComponent],
   imports: [
     CommonModule,
     RouterModule,
-    SignuploginRoutingModule
+    SignuploginRoutingModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     LoginComponent,
