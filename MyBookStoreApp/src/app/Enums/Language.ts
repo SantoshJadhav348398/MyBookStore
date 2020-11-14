c59a695b0d364a453a1615d018a476cc3cd3fbe2
Santosh <axelblaze348398@gmail.com>
2020-11-15 01:39:09 +0530

    Enums and userName call

---
+++ MyBookStoreApp/src/app/Enums/Language.ts
@@ -0,0 +1,23 @@
+/*
+ * *****************************************               #
+ * File : Language.ts                                      #
+ * FileCreated : Sunday, 15th November 2020, 1:07:48 am    #
+ * Description :                                           #
+ * ----------------------                                  #
+ * Author : Santosh                                        #
+ * Project : my-book-store-app                             #
+ * Last Modified: Sunday, 15th November 2020 1:07:48 am    #
+ * Modified By: Santosh (you@you.you>)                     #
+ *                                                         #
+ * ----------------------                                  #
+ * Copyright (c) 2020 Your Company                         #
+ * *****************************************               #
+ */
+
+
+ export enum Language {
+     English = "en-US",
+     German = "de-DE",
+     Hindi = "hi-HI",
+     Chinese = "cz-ch"
+ }
