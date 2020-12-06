a4514e23399b7257d221e68bd889b20c5030ce41
Reena <94reena.singh@gmail.com>
2020-12-06 20:58:55 +0530

    added header in shared module

---
+++ MyBookStoreApp/src/app/shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatIconModule
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class SharedModule { }
